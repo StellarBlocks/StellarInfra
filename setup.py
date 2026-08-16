7580bc9e0e6ff624760745a61ffb12f0c87f0aa0
PowerfulBean <25721564+powerfulbean@users.noreply.github.com>
2026-08-16 18:33:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
   name="StellarInfra",
-  version="0.0.8",
+  version="0.0.10",
   author="Powerfulbean",
   author_email="powerfulbean@gmail.com",
   description="An python infrastructure of StellarBlocks's software",
